dec32d6766d517f78389fe8b342e57dc9a20d856
Arnaud Izard <arnaud.izard@trackinsight.com>
2025-12-14 21:36:26 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState,useEffect  } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -9,10 +9,19 @@ import fields_summary from '../data/fields_summary.json'
 import { createPortal } from 'react-dom'
 
 
+function download ({content, filename="data.csv", type="text/csv;charset=utf-8;"}) {
+  const blob = new Blob([content], { type });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = filename;
+  // Trigger download
+  a.click();
+  URL.revokeObjectURL(url);
+}
+
 function Badge ({bg='bg-blue-300/20', border='border-blue-400', children}) {
-  
-  
-  return <div className={` inline-block  text-xs px-1 rounded ${bg} border ${border}`}>{children}</div>
+    return <div className={` inline-block  text-xs px-1 rounded ${bg} border ${border}`}>{children}</div>
 }
 classification_controls.map(cc => {
   const v = {
@@ -62,6 +71,13 @@ function App() {
     // }
     if (summary) {
       field.values = _.orderBy(summary.values, row => -row.count)
+      field.values.map(row => {
+        if (field.oasis_fields.oasis_field_values) {
+          const field_v = field.oasis_fields.oasis_field_values.find(v => v.value == row.value)
+          row.description = field_v?.description
+        }
+      })
+    
       field.sample = _.orderBy(summary.sample, row => -row.count)
     }
   })
@@ -69,12 +85,38 @@ function App() {
   const fieldGroups = _.uniqBy(fields.map(f => f.oasis_fields.domain ));
 
   const [currentField, setCurrentField] = useState(fields[10])
+
+  function downloadFields () {
+    const schema = [
+      {colname:'oasis_fields.domain',label:'domain'},
+      {colname:'code',label:'key'},
+      {colname:'label',label:'label'},
+      {colname:'oasis_fields.type',label:'type'},
+      {colname:'oasis_fields.is_array',label:'is_array'},
+      {colname:'oasis_fields.short_description',label:'short_description'},
+    ];
+    const csvContent = [schema.map( s => s.label ?? s.colname).join(';')];
+    fields.map( f => {
+      const line = schema.map(s => {
+        const v = _.get(f,s.colname) ?? "";
+        return `"${v}"`
+      }).join(';')
+      return csvContent.push(line);
+    })
+    download({content:csvContent.join('\n'), filename:'fields.csv'});
+  }
+
   return (
-    <div className="h-full flex flex-col text-left min-w-[1280px] w-[1480px] m-auto ">
+    <div className="h-full flex flex-col text-left w-[1260px] m-auto ">
       
-      <div className='py-4 px-4 gap-4 border-b border-gray-200'>
-        <div className="text-xl font-bold"> Trackinsigt Standard API </div>
-        <div className="font-bold"> Data Dictionary </div>
+      <div className='py-4 px-4 gap-4 border-b border-gray-200 flex'>
+        <div className="">
+          <div className="text-xl font-bold"> Trackinsigt Standard API </div>
+          <div className="font-bold"> Data Dictionary </div>
+        </div>
+        <div className="flex-grow"></div>
+        <div className="text-gray-600 hover:text-black underline cursor-pointer" onClick={() => downloadFields()}>fields.csv</div>
+
       </div>
       
       <div className="flex-grow min-h-0 ">
@@ -83,7 +125,7 @@ function App() {
 
           <div className="h-full w-[360px] min-w-[360px] p-4 flex flex-col ">
             <div className="py-3 mr-7">
-              <input name="filter" className="w-full py-2 px-3 border  border-gray-200 rounded-sm" type='text' placeholder="filter"
+              <input name="filter" className="w-full py-2 px-3 border  border-gray-200 rounded-sm" type='text' placeholder="Filter"
                 value={query}
                 onChange={(e) => setQuery(e.target.value)} ></input>
             </div>
@@ -159,17 +201,25 @@ function Details ({field,setModalContent, setModalOpen}) {
   };
 
   const [includingDelisted, setIncludingDelisted] = useState(false)
-  const values = {
+  field.values?.map(row => {
+    row.value_2 = <div className='flex gap-2'>
+      <div> <Display value={row.value}></Display> </div>
+      {row.description ? <div className="px-2 text-gray-400 text-xs bg-gray-100 border border-gray-300 rounded-xl"> ? </div> : ""}
+    </div>
+    
+
+  })
+  const values = { 
     rows:field.values,
     schema:[
-      { colname:'value', label:"Value"},
+      { colname:'value_2', label:"Value", export_colname:"value"},
       { colname:'total_count', label:"Count", align:'text-right'}
     ]
   };
   const listed_values = {
     rows:field.values?.filter( v => v.listed_count > 0),
     schema:[
-      { colname:'value', label:"Value"},
+      { colname:'value_2', label:"Value", export_colname:"value"},
       { colname:'listed_count', label:"Count", align:'text-right'}
     ]
   }
@@ -196,15 +246,26 @@ function Details ({field,setModalContent, setModalOpen}) {
       { colname:'share_label', label:"Share Label"},
       { colname:'column_value', label:"Column Value"},
     ]
+
+    console.log(row)
+    let content = null;
+    if (row.description) {
+      content = <DocRenderer data={row.description}></DocRenderer>
+    }
+
     const el = <div className="p-4">
-      <div className="mb-12">
+      <div className="mb-6">
         <div className="px-2 py-1 font-bold text-xs">Value</div>
         <div className="px-2 ">{row.value}</div>
       </div>
-      <div className="">
+      <div className="mb-6">
         <div className="px-2 py-1 font-bold text-xs">Examples</div>
         <Table rows = {row.samples} schema={schema}></Table>
       </div>
+      { content==null ? "" : <div className="">
+        <div className="px-2 py-1 font-bold text-xs">Additional Information</div>
+        <div className="px-2 text-sm">{content}</div>
+      </div>}
     </div>
     setModalContent(el)
     setModalOpen(true)
@@ -233,10 +294,10 @@ function Details ({field,setModalContent, setModalOpen}) {
            : <div className="">{_.get(field,'oasis_fields.type')}</div>
           }
         </div>
-        <div className="min-w-32">
+        {/* <div className="min-w-32">
           <div className="font-bold text-xs">Semantic Type</div>
           <div className="">{_.get(field,'oasis_fields.semantic_types.label')}</div>
-        </div>
+        </div> */}
       </div>
       
       <div className="flex gap-6 mb-6 border-t border-gray-200 pt-6">
@@ -307,32 +368,60 @@ function Details ({field,setModalContent, setModalOpen}) {
 
 }
 
+function Display({value}) {
+  if (value===null)  return <span className={"text-gray-400 italic"}> null</span>
+  if (value===undefined)  return <span className={" text-gray-400 italic"}> undefined</span>
+  if (value==="")  return <span className={" text-gray-400 italic"}> empty string</span>
+  if (value==="Multiple Values")  return <span className={" text-gray-400 italic"}> Multiple Values</span>
+  if (value===false)  return <span className={" text-gray-400 italic"}> false</span>
+  if (value===true)  return <span className={" text-gray-400 italic"}> true</span>
+  else return <span>{value}</span>;
+}
+
 function Table ({rows, schema, pageSize=10, onRowClicked}) {
 
   function downloadCSV () {
     const csvContent= [schema.map(s => s.label).join(',')];
     rows.map(row => {
-      csvContent.push(schema.map(s => row[s.colname]).join(','));
+      csvContent.push(schema.map(s => {
+        return row[s.export_colname ?? s.colname]
+      }).join(','));
     })
-    const blob = new Blob([csvContent.join('\n')], { type: "text/csv;charset=utf-8;" });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = "data.csv";
-    // Trigger download
-    a.click();
-    URL.revokeObjectURL(url);
-  
-  
+    download({content:csvContent.join('\n')});
   }
 
-  rows.map((row,i) => row.i =i);
-  const maxPage = Math.ceil(rows.length / pageSize);
-  const pages = [];
-  while (pages.length < maxPage) pages.push(pages.length + 1);
 
+  const [query,setQuery] = useState(null);
+  const [filteredRows, setFilteredRows] = useState(rows);
+  const [maxPage, setMaxPage] = useState(1);
+  const [pages, setPages] = useState([1]);
   const [page, setPage] = useState(1);
   const [pageData, setPageData] = useState(_.take(_.drop(rows,(page-1) * pageSize),pageSize));
+
+  useEffect(() => {
+    const newFilteredRows = rows.filter(row => {
+        if (query == null || query=='') return true;
+        for (const col of schema) {
+          
+          if (row[col.export_colname ?? col.colname]?.toString()?.toLowerCase()?.includes(query?.toLowerCase())) return true;
+        }
+        return false
+      })
+
+    const newMaxPage = Math.ceil(newFilteredRows.length / pageSize)
+    const newPages = [];
+    while (newPages.length < newMaxPage) newPages.push(newPages.length + 1);
+    
+    setMaxPage(newMaxPage);
+    setPages(newPages);
+    setFilteredRows(newFilteredRows)
+    const newPageData = _.take(_.drop(newFilteredRows,(page-1) * pageSize),pageSize);
+    setPageData(newPageData);
+
+  }, [query])
+
+  
+
 
   const nextPage = () => {
     if (page >= maxPage) return;
@@ -350,44 +439,55 @@ function Table ({rows, schema, pageSize=10, onRowClicked}) {
     setPageData(_.take(_.drop(rows, (page-1) * pageSize), pageSize));
   }
 
-  return <div className="border border-gray-200 rounded-md text-sm">
-    <table className="w-full">
-      <thead>
-        <tr>
-          {schema.map(col => {
-            return <th className={col.align} >{col.label}</th>
-          })}
-        </tr>
-      </thead>
-      <tbody>
-        {pageData.map(row => {
-          return <tr className="cursor-pointer hover:bg-gray-100/50 text-xs" onClick={() => onRowClicked && onRowClicked(row)}>
+  return <div>
+      <input name="filter" className="px-1 py-1 border mt-3 mb-1 outline-w-1 border-gray-200 rounded-sm" type='text' placeholder="Filter"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)} ></input> 
+      <div className="border border-gray-200 rounded-md text-sm">
+      <table className="w-full">
+        <thead>
+          <tr>
             {schema.map(col => {
-              if (row[col.colname]===null)  return <td className={col.align +" text-gray-400 italic"}> null</td>
-              if (row[col.colname]===undefined)  return <td className={col.align +" text-gray-400 italic"}> undefined</td>
-              if (row[col.colname]==="")  return <td className={col.align +" text-gray-400 italic"}> empty string</td>
-              if (row[col.colname]==="Multiple Values")  return <td className={col.align +" text-gray-400 italic"}> Multiple Values</td>
-              if (row[col.colname]===false)  return <td className={col.align +" text-gray-400 italic"}> false</td>
-              if (row[col.colname]===true)  return <td className={col.align +" text-gray-400 italic"}> true</td>
-              return <td className={col.align}> {row[col.colname]}</td>
+              return <th className={col.align} >{col.label}</th>
             })}
           </tr>
-        })}
-      </tbody>
-    </table>
-    <div className="flex gap-2 p-2 text-xs bg-gray-100">
-        <div className=''>{rows.length} rows</div>
-        <div className="hover:underline cursor-pointer" onClick={() => downloadCSV()}>Download .csv</div>
-        <div className='flex-grow'></div>
-        <div className="hover:border-gray-400 border border-gray-100 rounded-sm cursor-pointer  px-1" onClick={() =>  prevPage()}>Prev</div>
-        {pages.map(p => {
-          return <div className={`hover:border-gray-400 border border-gray-100 rounded-sm cursor-pointer px-1 ${p == page ? 'bg-gray-200' : ''}`} onClick={() => goToPage(p)}>{p}</div>
-        })}
-        <div className="hover:border-gray-400 border border-gray-100 rounded-sm cursor-pointer  px-1"  onClick={() =>  nextPage()}>Next</div>
+        </thead>
+        <tbody>
+          {pageData
+          .map(row => {
+            
+            return <tr className="cursor-pointer hover:bg-gray-100/50 text-xs" onClick={() => onRowClicked && onRowClicked(row)}>
+              {schema.map(col => {
+                return <td className={col.align}>{row[col.colname]}
+                </td>
+              })}
+            </tr>
+          })}
+        </tbody>
+      </table>
+      <div className="flex gap-2 p-2 text-xs bg-gray-100">
+          <div className=''>{rows.length} rows</div>
+          <div className="hover:underline cursor-pointer" onClick={() => downloadCSV()}>Download .csv</div>
+          <div className='flex-grow'></div>
+          <div className="hover:border-gray-400 border border-gray-100 rounded-sm cursor-pointer  px-1" onClick={() =>  prevPage()}>Prev</div>
+          {pages.map(p => {
+            return <div className={`hover:border-gray-400 border border-gray-100 rounded-sm cursor-pointer px-1 ${p == page ? 'bg-gray-200' : ''}`} onClick={() => goToPage(p)}>{p}</div>
+          })}
+          <div className="hover:border-gray-400 border border-gray-100 rounded-sm cursor-pointer  px-1"  onClick={() =>  nextPage()}>Next</div>
 
+      </div>
     </div>
   </div>
 
+}
+
+function DocRenderer({data}) {
+  
+  return <div>
+    {data.blocks.map(block => {
+      return <div className="py-1" dangerouslySetInnerHTML={{ __html: block.data?.text }}/>
+    })}
+  </div>
 }
 
 export default App
